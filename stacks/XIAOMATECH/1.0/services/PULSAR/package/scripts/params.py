2780e0ab15cbbb8575c18ae04a45c33c1eae9356
xianhuawei <xianhuawei.zhcn@qq.com>
2019-07-08 16:55:46 +0800

    'init'

---
+++ stacks/XIAOMATECH/1.0/services/PULSAR/package/scripts/params.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from resource_management.libraries.script import Script
+from resource_management.core.resources.system import Execute
+from resource_management.libraries.functions.default import default
+import os
+
+config = Script.get_config()
+
+pulsar_user = config['configurations']['pulsar-env']['pulsar_user']
+
+hostname = config['agentLevelParams']['hostname']
+java64_home = config['ambariLevelParams']['java_home']
+
+pulsar_content = config['configurations']['pulsar-env'][
+    'pulsar_content']
+
+
+conf_dir = '/etc/pulsar'
+
+user_group = config['configurations']['cluster-env']['user_group']
+
+download_url_base = default("/configurations/cluster-env/download_url_base",
+                            'http://assets.example.com/')
+
